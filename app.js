33decb0890e2505a901d230357f9417f82441758
MG <mginzburgs@me.com>
2024-08-03 21:50:03 +0300

    complete Etch-a-Sketch

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const dialog = document.querySelector("dialog");
+const body = document.querySelector(".main");
+const dialogButton = document.querySelector(".dialog-button");
 
 // ELEMENTS
 function createSquareGrid(amount) {
+  container.innerHTML = "";
   for (i = 0; i < amount; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
@@ -9,10 +13,30 @@ function createSquareGrid(amount) {
   }
 }
 
+function createGridButton() {
+  let GridButton = document.createElement("button");
+  GridButton.classList.add("new-grid-button");
+  GridButton.innerText = "CREATE NEW GRID";
+  GridButton.onclick = () => {
+    dialog.show();
+  };
+  body.prepend(GridButton);
+}
+
+function handleSubmit() {
+  let gridSize = document.querySelector("#grid-size-input").value;
+  if (value <= 1000) {
+    createSquareGrid(gridSize);
+    dialog.close();
+  }
+}
+
 // FUNCTIONS
 function colorSquaresOnHover() {
   container.addEventListener("mouseover", (e) => {
-    e.target.classList.toggle("square--on");
+    if (e.target.classList.contains("square")) {
+      e.target.classList.toggle("square--on");
+    }
   });
 }
 
@@ -20,6 +44,7 @@ function colorSquaresOnHover() {
 const buildApp = () => {
   createSquareGrid(256);
   colorSquaresOnHover();
+  createGridButton();
 };
 
 buildApp();
